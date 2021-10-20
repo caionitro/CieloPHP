893a820f10da400851de4a266cb99ec31f1f6abb
Leonardo de Souza Martins <leonardosmleo@gmail.com>
2021-10-20 11:02:17 -0300

    hack
    hack

--- includes/include.php
+++ includes/include.php
@@ -23,7 +23,7 @@ define("CIELO", "1001734898");
 define("CIELO_CHAVE", "e84827130b9837473681c2787007da5914d6359947015a5cdb2b8843db0fa832");
 
 
-// Envia requisição
+// Envia requisiÃ§Ã£o
 function httprequest($paEndereco, $paPost){
 
 	$sessao_curl = curl_init();
@@ -39,22 +39,22 @@ function httprequest($paEndereco, $paPost){
 	curl_setopt($sessao_curl, CURLOPT_SSL_VERIFYHOST, 2);
 
 	//  CURLOPT_SSL_CAINFO
-	//  informa a localização do certificado para verificação com o peer
+	//  informa a localizaÃ§Ã£o do certificado para verificaÃ§Ã£o com o peer
 	curl_setopt($sessao_curl, CURLOPT_CAINFO, getcwd() .
 			"/ssl/VeriSignClass3PublicPrimaryCertificationAuthority-G5.crt");
 	curl_setopt($sessao_curl, CURLOPT_SSLVERSION, 3);
 
 	//  CURLOPT_CONNECTTIMEOUT
-	//  o tempo em segundos de espera para obter uma conexão
+	//  o tempo em segundos de espera para obter uma conexÃ£o
 	curl_setopt($sessao_curl, CURLOPT_CONNECTTIMEOUT, 10);
 
 	//  CURLOPT_TIMEOUT
-	//  o tempo máximo em segundos de espera para a execução da requisição (curl_exec)
+	//  o tempo mÃ¡ximo em segundos de espera para a execuÃ§Ã£o da requisiÃ§Ã£o (curl_exec)
 	curl_setopt($sessao_curl, CURLOPT_TIMEOUT, 40);
 
 	//  CURLOPT_RETURNTRANSFER
 	//  TRUE para curl_exec retornar uma string de resultado em caso de sucesso, ao
-	//  invés de imprimir o resultado na tela. Retorna FALSE se há problemas na requisição
+	//  invÃ©s de imprimir o resultado na tela. Retorna FALSE se hÃ¡ problemas na requisiÃ§Ã£o
 	curl_setopt($sessao_curl, CURLOPT_RETURNTRANSFER, true);
 
 	curl_setopt($sessao_curl, CURLOPT_POST, true);
@@ -98,5 +98,4 @@ function ReturnURL()
 
 	return $ReturnURL;
 }
-
 ?>
